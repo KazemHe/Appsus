290a381ea7dd84348a8ac654a8c55c62bf6d4e5a
KazemHe <kazemhe1@gmail.com>
2023-03-04 21:17:08 +0200

    books

--- views/HomePage.js
+++ views/HomePage.js
@@ -1,7 +1,26 @@
+
+import EmailIndex from '../apps/mail/pages/EmailIndex.js'
+import NoteIndex from '../apps/keep/pages/NoteIndex.js'
+import BookInsex from '../books/pages/BookIndex.js'
+
 export default {
 	template: `
         <section class="home-page">
-            <h1>Home sweet home</h1>
+        <img  class="hp-img" src="../assets/style/img/mis books.jpg">
+           
+            <RouterLink  to="/email"><img  class="hp-img" src="../assets/style/img/meEmail.png"></RouterLink>
+            <RouterLink  to="/keep"> <img class="hp-img" src="../assets/style/img/keep.png"></RouterLink>
+
         </section>
     `,
+
+
+
+component :{
+    EmailIndex,
+    NoteIndex,
+    BookInsex,
+
 }
+}
+
